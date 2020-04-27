0b3b7f3a760d757685b24eb0e03cdbac9207a51b
Karlos Naidas <krnaidas@gmail.com>
2020-04-27 15:35:38 +0800

    fixed UI AGAIN

--- Covid19-Tracker/ViewController.swift
+++ Covid19-Tracker/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var searchFieldText: UITextField!
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var updatedLabel: UILabel!
+    
+    @IBOutlet weak var currentCasesLabel: UILabel!
+    @IBOutlet weak var currentDeathsLabel: UILabel!
+    
+    @IBOutlet weak var newCasesLabel: UILabel!
+    @IBOutlet weak var newRecoveriesLabel: UILabel!
+    @IBOutlet weak var newDeathsLabel: UILabel!
+    @IBOutlet weak var totalTestsLabel: UIStackView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func searchedPressed(_ sender: UITextField) {
+    }
+    
+    @IBAction func searchFieldPressed(_ sender: UITextField) {
+    }
+    @IBAction func globeButtonPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
